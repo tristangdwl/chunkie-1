622140a28a0d1ea5c896507c6d240d0dc61a6ec2
peter-nekrasov <pn3@uchicago.edu>
2025-08-05 20:12:56 -0500

    clamped kernels working, rest tbd

--- chunkie/demo/test_flex_bc_kern.m
+++ chunkie/demo/test_flex_bc_kern.m
@@ -1,3 +1,5 @@
+
+
 phi = 0.3;
 src = []; src.r = [0;0]; src.n = [cos(phi);sin(phi)]; src.d = [cos(phi+pi/2);sin(phi+pi/2)];
 
@@ -13,52 +15,85 @@ zk = 0.9; nu = 0.3;
 % nu = 2;
 h = 1e-3;
 
-ifree = 1;
+ifree = 0;
 
-%%
+%% checking all kernels in the clamped BCs
+
 targh_n = [];
 targh_n.r = targ.r + h*[1,0,-1].*targ.n;
 targh_n.n = targ.n + 0*[1,0,-1];
 targh_n.d = targ.d + 0*[1,0,-1];
 
-if ifree
-ikern = @(s,t) chnk.flex2d.kern(zk, s, t, 'free_plate_eval', nu); 
-ikern_1 = @(s,t) chnk.flex2d.kern(zk, s, t, 'free_to_clamped', nu); 
-else
-ikern = @(s,t) chnk.flex2d.kern(zk, s, t, 'clamped_plate_eval', nu); 
-ikern_1 = @(s,t) chnk.flex2d.kern(zk, s, t, 'clamped_plate_bc1', nu); 
-end
-
-% ikern = @(s,t) chnk.flex2d.kern(zk, s, t, 's', nu); 
-% ikern_1 = @(s,t) chnk.flex2d.kern(zk, s, t, 'sprime', nu);
+ikern = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'free_plate_eval'); 
+ikern_1 = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'free_to_clamped'); 
 
 u1 = ikern_1(src,targ);
 
 uh= ikern(src,targh_n); u1_fd = (uh(1,:)-uh(3,:))/2/h;
-[u1;u1_fd;abs(u1_fd-u1)]
-assert(norm(u1_fd-u1)<1e-6)
 
-% return
-%%
+assert(norm(u1(1,:)-uh(2,:))<1e-9)
+assert(norm(u1(2,:) - u1_fd)<1e-6)
+
+ikern = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'clamped_plate_eval'); 
+ikern_1 = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'clamped_plate'); 
+
+u1 = ikern_1(src,targ);
+
+uh= ikern(src,targh_n); u1_fd = (uh(1,:)-uh(3,:))/2/h;
+
+assert(norm(u1(1,:)-uh(2,:))<1e-9)
+assert(norm(u1(2,:) - u1_fd)<1e-6)
+
+ikern = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 's'); 
+ikern_1 = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'clamped_plate_bcs'); 
+
+u1 = ikern_1(src,targ);
+
+uh= ikern(src,targh_n); u1_fd = (uh(1,:)-uh(3,:))/2/h;
+
+assert(norm(u1(1,:)-uh(2,:))<1e-9)
+assert(norm(u1(2,:) - u1_fd)<1e-6)
+
+return
+
+%% checking all kernels in the free plate BC 1
+
 targh_lap = [];
 targh_lap.r = targ.r + h*[1,0,-1,0,0].*targ.n+h*[0,0,0,1,-1].*targ.d;
 targh_lap.n = targ.n + 0*[1,0,-1,0,0];
 targh_lap.d = targ.d + 0*[1,0,-1,0,0];
 targh_lap.d2 = targ.d2 + 0*[1,0,-1,0,0];
 
-if ifree
-ikern_2 = @(s,t) chnk.flex2d.kern(zk, s, t, 'free_plate_bc2', nu); 
-else
-ikern_2 = @(s,t) chnk.flex2d.kern(zk, s, t, 'clamped_plate_bc2', nu); 
-end
-% ikern_2 = @(s,t) chnk.flex2d.kern(zk, s, t, 'supported_plate_bcs', nu); 
-u2 = ikern_2(src,targ);%u2 = u2(2);
+ikern = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'clamped_plate_eval'); 
+ikern_2 = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'clamped_to_free');
+
+u2 = ikern_2(src,targ);
 
 uh= ikern(src,targh_lap); 
-u2_fd = (1-nu)*(uh(1,:)-2*uh(2,:)+uh(3,:))/h/h + ...
-    nu*(uh(1,:)-4*uh(2,:)+uh(3,:)+uh(4,:)+uh(5,:))/h/h;
-[u2;u2_fd;abs(u2_fd-u2)]
-assert(norm(u2_fd-u2)<1e-6)
+u2_fd = (uh(1,:)-4*uh(2,:)+uh(3,:)+uh(4,:)+uh(5,:))/h/h + zk^2*uh(2,:);
+
+assert(norm(u1(1,:)-uh(2,:))<1e-9)
+assert(norm(u1(2,:) - u1_fd)<1e-6)
+
+ikern = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'clamped_plate_eval'); 
+ikern_1 = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'clamped_plate'); 
+
+u1 = ikern_1(src,targ);
+
+uh= ikern(src,targh_n); u1_fd = (uh(1,:)-uh(3,:))/2/h;
+
+assert(norm(u1(1,:)-uh(2,:))<1e-9)
+assert(norm(u1(2,:) - u1_fd)<1e-6)
+
+ikern = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 's'); 
+ikern_2 = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'free_plate_bcs', nu); 
+
+u2 = ikern_2(src,targ);
+
+uh= ikern(src,targh_lap); 
+u2_fd = (uh(1,:)-4*uh(2,:)+uh(3,:)+uh(4,:)+uh(5,:))/h/h + zk^2*uh(2,:);
+
+assert(norm(u2_fd-u2(1,:))<1e-6)
 
 
 %%
