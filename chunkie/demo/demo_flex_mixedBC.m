2a58c129d5eff3068d82806a8746eb355c778e41
Tristan Goodwill <48371600+tristangdwl@users.noreply.github.com>
2025-08-05 20:14:30 -0500

    begin demo

--- chunkie/demo/demo_flex_mixedBC.m
+++ chunkie/demo/demo_flex_mixedBC.m
@@ -5,7 +5,6 @@
 %
 
 zk = 10;
-nu = 0.3;
 
 % discretize domain
 
@@ -24,9 +23,6 @@ start = tic;
 chnkr_f = chunkerfunc(@(t) starfish(t,narms1,amp,[0;4],[],1),cparams,pref); 
 chnkr_c = chunkerfunc(@(t) starfish(t,narms2,amp),cparams,pref); 
 
-% chnkr_f = chunkerfunc(@(t) starfish(t,narms1,amp,[0;0.5],[],1),cparams,pref); 
-% chnkr_c = chunkerfunc(@(t) starfish(t,narms2,amp,[],[],3),cparams,pref); 
-% chnkr_c = reverse(chnkr_c);
 chnkr = merge([chnkr_f,chnkr_c]);
 t1 = toc(start);
 
@@ -44,32 +40,32 @@ quiver(chnkr)
 axis equal
 
 %% Get system matrix and right hand side
-ibc = 1;
+ibc = 0;
 
 start = tic;
 if ibc == 2
-[sys,H_f] = mix_sysmat(chnkr_f,chnkr_c,zk,nu);
+sys = mix_sysmat(chnkr_f,chnkr_c,zk,1,1);
 elseif ibc == 1
-[sys,H_f] = free_sysmat(chnkr,zk,nu);
+sys = free_sysmat(chnkr,zk,1);
 else
-[sys] = clamped_sysmat(chnkr,zk);
+sys = clamped_sysmat(chnkr,zk,1);
 end
 t1 = toc(start);
 fprintf('%5.2e s : time to assemble matrix\n',t1)
 
 % building RHS
-src =[]; src.r = [1;2];
-% src =[]; src.r = [0.5;0];src.r = [0;4.5];
-free_bc_kern = @(s,t) chnk.flex2d.kern(zk, s, t, 'free_plate_bcs', nu);
-clamp_bc_kern = @(s,t) chnk.flex2d.kern(zk, s, t, 'clamped_plate_bcs', nu);
+% src =[]; src.r = [1;2];
+src =[]; src.r = [0.5;0];%src.r = [0;4.5];
+free_bc_kern = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'free_plate_bcs');
+clamp_bc_kern = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'clamped_plate_bcs');
 
-flex_free_kern = @(s,t) chnk.flex2d.kern(zk, s, t, 's');
+flex_free_kern = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 's');
 
-% get free plate BCs
-rhs_f = -free_bc_kern(src,chnkr_f);
-% get clamped plate BCs
-rhs_c = -clamp_bc_kern(src,chnkr_c);
-rhs = [rhs_f;rhs_c];
+% % get free plate BCs
+% rhs_f = -free_bc_kern(src,chnkr_f);
+% % get clamped plate BCs
+% rhs_c = -clamp_bc_kern(src,chnkr_c);
+% rhs = [rhs_f;rhs_c];
 
 if ibc == 1
 rhs = -free_bc_kern(src,chnkr);
@@ -109,9 +105,9 @@ fprintf('%5.2e s : time to find points in domain\n',t1)
 
 start1 = tic;
 if ibc == 2
-uscat = mixed_eval(chnkr_f,chnkr_c,sol,targets(:,out),H_f,zk,nu);
+uscat = mixed_eval(chnkr_f,chnkr_c,sol,targets(:,out),zk);
 elseif ibc == 1
-uscat = free_eval(chnkr,sol,targets(:,out),H_f,zk,nu);
+uscat = free_eval(chnkr,sol,targets(:,out),zk);
 else
 uscat = clamp_eval(chnkr,sol,targets(:,out),zk);
 end
@@ -235,98 +231,52 @@ title('$u^{\textrm{tot}}$','Interpreter','latex','FontSize',12)
 colorbar
 % %%
 % sol_f = sol(1:2*chnkr_f.npt);
-% sys_fc = free2clamp_sysmat(chnkr_f,chnkr_c,H_f,zk,nu);
-% u2 = free_eval(chnkr_f,sol_f,chnkr_c,H_f,zk,nu);
+% sys_fc = free2clamp_sysmat(chnkr_f,chnkr_c,zk);
+% u2 = free_eval(chnkr_f,sol_f,chnkr_c,zk);
 % norm(u2-sys_fc(1:2:end,:)*sol_f)
 
-function [sys,H_f] = mix_sysmat(chnkr_f,chnkr_c,zk,nu)
+function sys = mix_sysmat(chnkr_f,chnkr_c,zk,sgn_c,sgn_f)
 % build the block system matrix
-    [sys_ff,H] = free_sysmat(chnkr_f,zk,nu);
-    sys_fc = free2clamp_sysmat(chnkr_f,chnkr_c,H,zk,nu);
-    sys_cf = clamp2free_sysmat(chnkr_f,chnkr_c,zk,nu);
-    sys_cc = clamped_sysmat(chnkr_c,zk);
+    sys_ff = free_sysmat(chnkr_f,zk,sgn_f);
+    sys_fc = free2clamp_sysmat(chnkr_f,chnkr_c,zk);
+    sys_cf = clamp2free_sysmat(chnkr_f,chnkr_c,zk);
+    sys_cc = clamped_sysmat(chnkr_c,zk,sgn_c);
 
     sys = [sys_ff,sys_cf;sys_fc,sys_cc];
-    H_f = H;
 end
 
 
-function [sys,H] = free_sysmat(chnkr,zk,nu)
+function sys = free_sysmat(chnkr,zk,sgn)
     % assembling system matrix for free boundary
 
-    % defining free plate kernels
-    
-    fkern1 =  @(s,t) chnk.flex2d.kern(zk, s, t, 'free_plate', nu);        % build the desired kernel
-    double = @(s,t) chnk.lap2d.kern(s,t,'d');
-    hilbert = @(s,t) chnk.lap2d.kern(s,t,'hilb');
-    
-    opts = [];
-    opts.sing = 'log';
-    
-    opts2 = [];
-    opts2.sing = 'pv';
-    
-    % building system matrix
-    
-    sysmat1 = chunkermat(chnkr,fkern1, opts);
-    D = chunkermat(chnkr, double, opts);
-    H = chunkermat(chnkr, hilbert, opts2);     
-    
-    sysmat = zeros(2*chnkr.npt);
-    sysmat(1:2:end,1:2:end) = sysmat1(1:4:end,1:2:end) + sysmat1(3:4:end,1:2:end)*H  - 2*((1+nu)/2)^2*D*D;
-    sysmat(2:2:end,1:2:end) = sysmat1(2:4:end,1:2:end) + sysmat1(4:4:end,1:2:end)*H;
-    sysmat(1:2:end,2:2:end) = sysmat1(1:4:end,2:2:end) + sysmat1(3:4:end,2:2:end);
-    sysmat(2:2:end,2:2:end) = sysmat1(2:4:end,2:2:end) + sysmat1(4:4:end,2:2:end);
-    
-    D = [-1/2 + (1/8)*(1+nu).^2, 0; 0, 1/2];  % jump matrix 
-    D = kron(eye(chnkr.npt), D);
-    
-    sys =  D + sysmat;
-
+    sys = clamped_sysmat(chnkr,zk,sgn);
 end
 
-function sys = free2clamp_sysmat(chnkr_f,chnkr_c,H,zk,nu)
+function sys = free2clamp_sysmat(chnkr_f,chnkr_c,zk)
     % assemble matrix for free boundary talking to clamped boundary
     
     % defining free plate kernels
-    fkern_0 = @(s,t) chnk.flex2d.kern(zk, s, t, 'free_plate_eval', nu); 
-    fkern_1 = @(s,t) chnk.flex2d.kern(zk, s, t, 'free_plate_bc1', nu); 
-    
+    fkern = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'free_to_clamped'); 
     % building system matrix
     
-    sysmat0 = chunkerkernevalmat(chnkr_f,fkern_0,chnkr_c);
-    sysmat1 = chunkerkernevalmat(chnkr_f,fkern_1,chnkr_c);
-    
-    sys = zeros(2*chnkr_c.npt,2*chnkr_f.npt);
-    
-    sys(1:2:end,1:2:end) = sysmat0(:,1:3:end) + sysmat0(:,2:3:end)*H;
-    sys(2:2:end,1:2:end) = sysmat1(:,1:3:end) + sysmat1(:,2:3:end)*H;
-    sys(1:2:end,2:2:end) = sysmat0(:,3:3:end);
-    sys(2:2:end,2:2:end) = sysmat1(:,3:3:end);
+    sys = chunkerkernevalmat(chnkr_f,fkern,chnkr_c);
 end
 
-function sys = clamp2free_sysmat(chnkr_f,chnkr_c,zk,nu)
+function sys = clamp2free_sysmat(chnkr_f,chnkr_c,zk)
     % assemble matrix for clamped boundary talking to free boundary
 
     % defining clamped plate kernels
-    fkern_0 = @(s,t) chnk.flex2d.kern(zk, s, t, 'clamped_plate_bc2', nu); 
-    fkern_1 = @(s,t) chnk.flex2d.kern(zk, s, t, 'clamped_plate_bc3', nu); 
+    fkern = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'clamped_to_free'); 
     
     % building system matrix
     
-    sysmat0 = chunkerkernevalmat(chnkr_c,fkern_0,chnkr_f);
-    sysmat1 = chunkerkernevalmat(chnkr_c,fkern_1,chnkr_f);
-    
-    sys = zeros(2*chnkr_f.npt,2*chnkr_c.npt);
-    
-    sys(1:2:end,:) = sysmat0;
-    sys(2:2:end,:) = sysmat1;
+    sys = chunkerkernevalmat(chnkr_c,fkern,chnkr_f);
 end
 
-function sys = clamped_sysmat(chnkr,zk)
+function sys = clamped_sysmat(chnkr,zk,sgn)
     % assembling system matrix for clamped boundary
     
-    fkern =  @(s,t) chnk.flex2d.kern(zk, s, t, 'clamped_plate');
+    fkern =  @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'clamped_plate');
     
     kappa = signed_curvature(chnkr);
     kappa = kappa(:);
@@ -335,35 +285,30 @@ function sys = clamped_sysmat(chnkr,zk)
     opts.sing = 'log';
     
     sys = chunkermat(chnkr,fkern, opts);
-    sys = sys - 0.5*eye(2*chnkr.npt);
-    sys(2:2:end,1:2:end) = sys(2:2:end,1:2:end) + kappa.*eye(chnkr.npt);
+    sys = sys - sgn*0.5*eye(2*chnkr.npt);
+    sys(2:2:end,1:2:end) = sys(2:2:end,1:2:end) + sgn*kappa.*eye(chnkr.npt);
 end
 
-function uscat = mixed_eval(chnkr_f,chnkr_c,sol,targets,H_f,zk,nu)
+function uscat = mixed_eval(chnkr_f,chnkr_c,sol,targets,zk)
     % evaluate layer potentials one BC at a time
     sol_f = sol(1:2*chnkr_f.npt);
     sol_c = sol((1+2*chnkr_f.npt):end);
 
-    uscat = free_eval(chnkr_f,sol_f,targets,H_f,zk,nu);
+    uscat = free_eval(chnkr_f,sol_f,targets,zk);
     uscat = uscat+clamp_eval(chnkr_c,sol_c,targets,zk);
 
 end
 
-function uscat = free_eval(chnkr,sol,targets,H,zk,nu)
+function uscat = free_eval(chnkr,sol,targets,zk)
     % evaluate layer potentials from free plate representation
-    ikern = @(s,t) chnk.flex2d.kern(zk, s, t, 'free_plate_eval', nu); 
+    ikern = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'free_plate_eval'); 
     
-    dens_comb = zeros(3*chnkr.npt,1);
-    dens_comb(1:3:end) = sol(1:2:end);
-    dens_comb(2:3:end) = H*sol(1:2:end);
-    dens_comb(3:3:end) = sol(2:2:end);
-    
-    uscat = chunkerkerneval(chnkr, ikern,dens_comb,targets);
+    uscat = chunkerkerneval(chnkr, ikern,sol,targets);
 end
 
 function uscat = clamp_eval(chnkr,sol,targets,zk)
     % evaluate layer potentials from clamped plate representation
-    ikern = @(s,t) chnk.flex2d.kern(zk, s, t, 'clamped_plate_eval');   
+    ikern = @(s,t) chnk.flex2d.kern_alt(zk, s, t, 'clamped_plate_eval');   
     
     uscat = chunkerkerneval(chnkr, ikern,sol,targets);
 end
